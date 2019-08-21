7bd4c7e7fab1b6f133fd83189bbd14d95358c484
Jacob Moss <jacobmoss09@gmail.com>
2019-08-20 21:23:25 -0500

    GET survey route established and showing survey html as expected

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express()
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+ 
+app.get('/survey', function (request, response) {
+  response.sendFile(path.join(__dirname, '../public/survey.html'))
+})
+ 
+app.listen(3000)
