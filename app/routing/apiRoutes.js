723ae890e0272e353f0fad201d471a610612bd45
Jacob Moss <jacobmoss09@gmail.com>
2019-08-23 20:00:45 -0500

    api/friends route returns data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,10 @@
-const express = require("express");
-const path = require("path");
 
-const app = express()
-var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.get("/api/friends", function(request, response) {
+//     return response.json(friends);
+//   });
 
-app.get("/api/friends", function(request, response) {
-    return response.json(friends);
-  });
-
-app.post("/api/friends", function(request, response){
-    const newFriend = request.body;
+// app.post("/api/friends", function(request, response){
+//     const newFriend = request.body;
     
-});
+// });
--- server.js
+++ server.js
@@ -2,12 +2,36 @@ const express = require('express');
 
 const path = require('path');
 
+const friends = require(path.join(__dirname,'/app/data/friends'));
+
 const app = express()
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/survey', function (request, response) {
+    response.sendFile(path.join(__dirname, '/app/public/survey.html'))
+});
+  
+app.get('/', function (request, response) {
+    response.sendFile(path.join(__dirname, '/app/public/home.html'))
+})
+
+app.get("/api/friends", function(request, response) {
+    return response.json(friends);
+});
+
+app.post("/api/friends", function(request, response){
+    const newFriend = request.body; 
+    console.log(newFriend);
+
+    characters.push(newFriend);
+  
+    res.json(newFriend);
+});
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
