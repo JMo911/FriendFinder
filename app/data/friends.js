ca2889479a7986dec5d52492f1d062aa4099f91a
Jacob Moss <jacobmoss09@gmail.com>
2019-08-24 12:41:20 -0500

    user diffs found

--- app/data/friends.js
+++ app/data/friends.js
@@ -62,7 +62,29 @@ function findMatch(name) {
     currentUser.responses.forEach(function(element){
         currentUserScores.push(parseInt(element));
     });
-    // console.log(currentUserScores);
+    //   console.log(currentUserScores);
+    //now compare current user scores to all other friends' scores
+
+
+
+
+    
+     friends.forEach(function(element){
+
+        if (element.name !== name) {
+            let comparisonArray = element.responses;
+            //  console.log(comparisonArray);
+            //for each comparison array i need to subtract current user responses to get a total difference
+            let totalDiff = 0;
+            for (let i = 0; i < currentUserScores.length; i++) {
+                //abs val
+                totalDiff= totalDiff + (Math.abs(currentUserScores[i]-comparisonArray[i]));
+            }
+            console.log(element.name + totalDiff);
+        }
+
+     })
+    
     
 
 }
